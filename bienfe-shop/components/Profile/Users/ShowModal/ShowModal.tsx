633f93ec1735f1c090f5c25ac985bfef2e28e969
nantenaina731 <Sarobidy2003@gmail.com>
2025-05-09 19:54:10 +0100

    modify

--- bienfe-shop/components/Profile/Users/ShowModal/ShowModal.tsx
+++ bienfe-shop/components/Profile/Users/ShowModal/ShowModal.tsx
@@ -68,7 +68,7 @@ const ShowModal = ({
           style={{ color: primary, fontWeight: "bold", textAlign: "center" }}
           variant="titleMedium"
         >
-          Information sur l'employé
+          Information sur le vendeur
         </Text>
         {userSelected && (
           <View>
@@ -110,7 +110,7 @@ const ShowModal = ({
                 style={{ color: "red", textDecorationLine: "underline" }}
                 variant="titleMedium"
               >
-                Supprimer l'employé
+                Supprimer vendeur
               </Text>
               {isDelete ? (
                 <AntDesign name="up" color={"red"} size={15} />
